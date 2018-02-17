4a10419113b65503819945aa8dc0a774575beea3
Adam Plumer <caerus.karu@gmail.com>
2018-02-17 16:40:48 +0000

    04b9bfd build: enforce that deprecated APIs have a deletion target

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -21,7 +21,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 /**
  * Current version of Angular Flex-Layout.
  */
-const VERSION = new Version('2.0.0-beta.12-ba38451');
+const VERSION = new Version('2.0.0-beta.12-04b9bfd');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -23,7 +23,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 /**
  * Current version of Angular Flex-Layout.
  */
-var VERSION = new Version('2.0.0-beta.12-ba38451');
+var VERSION = new Version('2.0.0-beta.12-04b9bfd');
 
 /**
  * @fileoverview added by tsickle
