bdf636734eb3809ac7d06127e966047f6f5e6ef7
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-02-22 21:20:22 +0000

    b525771 build(release): allow version bumps to 6.0.0-beta.xxx

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -21,7 +21,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 /**
  * Current version of Angular Flex-Layout.
  */
-const VERSION = new Version('2.0.0-beta.12-1cf8a81');
+const VERSION = new Version('2.0.0-beta.12-b525771');
 
 /**
  * @fileoverview added by tsickle
