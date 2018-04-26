a099b0d42054d056a9902c5e8f308c51b81130ce
Teamop <zhangwenlp@vip.qq.com>
2018-04-26 15:25:25 +0000

    9689207 test(gap): fix test issue

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of Angular Flex-Layout.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-b8c86be');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-9689207');
 
 /**
  * @fileoverview added by tsickle
