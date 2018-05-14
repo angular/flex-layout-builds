03e1bb2ae785bf9a9917c70c0b81b7d30fe862b2
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-05-14 11:55:53 +0000

    086d038 update(doc): add install notes for Angular 6

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of Angular Flex-Layout.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-f1b3589');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-086d038');
 
 /**
  * @fileoverview added by tsickle
