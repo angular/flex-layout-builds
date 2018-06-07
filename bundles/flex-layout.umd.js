60e2cd28b85c9a3a92a52570591dbc60dfea0e02
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-06-07 22:26:44 +0000

    b72c498 update(doc): add Beta.16 release information

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /**
  * Current version of Angular Flex-Layout.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-666aa0a');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-beta.15-b72c498');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -23,7 +23,7 @@ export { ɵb, ɵc, ɵd, ɵe, ɵf, ɵg, ɵh, ɵi, ɵa, ɵj, ɵk, GridModule } fro
 /**
  * Current version of Angular Flex-Layout.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-beta.15-666aa0a');
+const /** @type {?} */ VERSION = new Version('6.0.0-beta.15-b72c498');
 
 /**
  * @fileoverview added by tsickle
