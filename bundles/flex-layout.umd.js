55c57b848198520d3c7c7cef028819de4855dd8f
null-reference <guyfrompei@gmail.com>
2018-11-28 17:40:38 +0000

    220344d docs: minor spelling fixes (#895)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.19-9148e87');
+var VERSION = new core.Version('7.0.0-beta.19-220344d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -23,7 +23,7 @@ export { ɵb, ɵc, ɵd, ɵe, ɵf, ɵg, ɵh, ɵi, ɵa, ɵj, ɵk, GridModule } fro
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.19-9148e87');
+var VERSION = new Version('7.0.0-beta.19-220344d');
 
 /**
  * @fileoverview added by tsickle
