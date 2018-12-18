5c9dde865f80fe344b1102e2bc7e9c7a284c3643
Adam Plumer <caerus.karu@gmail.com>
2018-12-18 03:33:35 +0000

    adam/beta22 - 90d6c81 chore: update to 7.0.0-beta.22 with changelog

--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -23,7 +23,7 @@ export { ɵf, ɵe, ɵd, ɵi, ɵh, ɵg, ɵl, ɵk, ɵj, ɵo, ɵn, ɵm, ɵr, ɵq, �
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.21-7759b6c');
+var VERSION = new Version('7.0.0-beta.22-90d6c81');
 
 /**
  * @fileoverview added by tsickle
--- typings/esm5/core/base/base.d.ts
+++ typings/esm5/core/base/base.d.ts
@@ -14,7 +14,7 @@ import { StyleBuilder } from '../style-builder/style-builder';
 /**
  * Abstract base class for the Layout API styling directives.
  * @deprecated
- * @deletion-target v7.0.0-beta.21-7759b6c
+ * @deletion-target v7.0.0-beta.21
  */
 export declare abstract class BaseDirective implements OnDestroy, OnChanges {
     protected _mediaMonitor: MediaMonitor;
