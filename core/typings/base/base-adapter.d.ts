63f7628b5d53ec630165b01e3e3ee4a8c7e142fb
CaerusKaru <caerus.karu@gmail.com>
2018-12-19 15:14:29 +0000

    master - ffd8331 feat(core): add static scss mixin (#940)

--- core/typings/base/base-adapter.d.ts
+++ core/typings/base/base-adapter.d.ts
@@ -15,7 +15,7 @@ import { StyleUtils } from '../style-utils/style-utils';
  * Adapter to the BaseDirective abstract class so it can be used via composition.
  * @see BaseDirective
  * @deprecated
- * @deletion-target v7.0.0-beta.21-1548727
+ * @deletion-target v7.0.0-beta.21-ffd8331
  */
 export declare class BaseDirectiveAdapter extends BaseDirective {
     protected _baseKey: string;
--- esm5/extended.es5.js
+++ esm5/extended.es5.js
@@ -275,7 +275,7 @@ var DefaultClassDirective = /** @class */ (function (_super) {
  *  - When 'hide' === '' === true, do NOT show the element
  *  - When 'hide' === false or 0... we WILL show the element
  * @deprecated
- * \@deletion-target v7.0.0-beta.21-1548727
+ * \@deletion-target v7.0.0-beta.21-ffd8331
  * @param {?} hide
  * @return {?}
  */
