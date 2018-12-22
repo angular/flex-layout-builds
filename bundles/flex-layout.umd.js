195fff20f6c798be7505f5f841e7c06ddf889f57
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-12-22 20:03:36 +0000

    thomas/fix-print - 21e68b5 feat(core): add print support with mediaQuery override

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -44,7 +44,7 @@ var __assign = function() {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.22-9c31ffa');
+var VERSION = new core.Version('7.0.0-beta.22-21e68b5');
 
 /**
  * @fileoverview added by tsickle
