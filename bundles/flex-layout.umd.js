51d7730257faaffe5016791c5f8e3f208c0af703
Adam Plumer <caerus.karu@gmail.com>
2018-12-28 07:05:16 +0000

    adam/fix-issue-813 - 988475e fixup! fix(style): add SSR capabilities

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.22-bce99a7');
+var VERSION = new core.Version('7.0.0-beta.22-988475e');
 
 /**
  * @fileoverview added by tsickle
