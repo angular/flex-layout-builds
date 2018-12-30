4287a46c3553ae0cd3ba80bf737d963151bd99b4
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-12-30 03:31:57 +0000

    thomas/fix-bp-priority - 765afe2 fix(core): improve use of breakpoint priorities

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.22-a2617da');
+var VERSION = new core.Version('7.0.0-beta.22-765afe2');
 
 /**
  * @fileoverview added by tsickle
