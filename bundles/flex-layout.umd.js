7ca3afff66e0a3e2f6c8c28966473aba51459974
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-01-01 17:02:24 +0000

    thomas/fix-print - 29b0c2d updates to watermark

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -44,7 +44,7 @@ var __assign = function() {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.22-b3af138');
+var VERSION = new core.Version('7.0.0-beta.22-29b0c2d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -24,7 +24,7 @@ export { ɵf, ɵe, ɵd, ɵi, ɵh, ɵg, ɵl, ɵk, ɵj, ɵo, ɵn, ɵm, ɵr, ɵq, �
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.22-b3af138');
+var VERSION = new Version('7.0.0-beta.22-29b0c2d');
 
 /**
  * @fileoverview added by tsickle
