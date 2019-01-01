998ca5bf738cd776859642ae96097067587d3bab
CaerusKaru <caerus.karu@gmail.com>
2019-01-01 06:44:40 +0000

    adam/year - 1e705de chore: bump-year

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -18,7 +18,7 @@
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.22-d57b293');
+var VERSION = new core.Version('7.0.0-beta.22-1e705de');
 
 /**
  * @fileoverview added by tsickle
