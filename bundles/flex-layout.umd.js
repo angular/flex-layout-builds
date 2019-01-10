c7b1c9edb7edd42663dd013e2c0b3b713952da18
Adam Plumer <caerus.karu@gmail.com>
2019-01-10 21:44:32 +0000

    adam/beta23 - af84045 chore: update to 7.0.0-beta.23 with changelog

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -44,7 +44,7 @@ var __assign = function() {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-7ffa210');
+var VERSION = new core.Version('7.0.0-beta.23-af84045');
 
 /**
  * @fileoverview added by tsickle
