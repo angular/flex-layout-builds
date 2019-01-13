9b0c7272f33175755e9fe328d2b64f832d3fc788
Adam Plumer <caerus.karu@gmail.com>
2019-01-13 23:05:39 +0000

    adam/mq - 5c73b4d fix(sass): update stylesheet with correct keys

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7373,7 +7373,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-c1fc857');
+var VERSION = new core.Version('7.0.0-beta.23-5c73b4d');
 
 /**
  * @fileoverview added by tsickle
