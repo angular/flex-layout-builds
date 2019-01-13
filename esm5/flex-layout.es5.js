cdcd8265d0134ad50f0cc249e9663a5fa83d9457
Adam Plumer <caerus.karu@gmail.com>
2019-01-13 07:15:23 +0000

    adam/gap - 2acb5e4 fix(fxLayoutGap): not working with dynamic fxHide

--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -24,7 +24,7 @@ export { ɵf2, ɵe2, ɵd2, ɵi2, ɵh2, ɵg2, ɵl2, ɵk2, ɵj2, ɵo2, ɵn2, ɵm2,
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new Version('7.0.0-beta.23-7d2db14');
+var VERSION = new Version('7.0.0-beta.23-2acb5e4');
 
 /**
  * @fileoverview added by tsickle
