237952f082c5d8b6515f48977e8e7e9851608812
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-01-13 19:41:21 +0000

    thomas/fix-issue-961 - 049610c feat(demo): add demo to show fxHide with used with custom breakpoint

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7340,7 +7340,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-a0d26e9');
+var VERSION = new core.Version('7.0.0-beta.23-049610c');
 
 /**
  * @fileoverview added by tsickle
