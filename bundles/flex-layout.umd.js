1d639c600c330d6e01254d8f52e11a01f864290f
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-01-14 02:56:57 +0000

    thomas/fix-imgsrc-test - 6411731 test(imgrsc): fix use of mediaController variable

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7373,7 +7373,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-a9c65f3');
+var VERSION = new core.Version('7.0.0-beta.23-6411731');
 
 /**
  * @fileoverview added by tsickle
