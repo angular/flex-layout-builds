c4b6883361f469d4d83a54cf40b1f03fe1969485
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-01-14 21:08:46 +0000

    thomas/feat-media-trigger - 5e54c69 feat(demo): add MediaTrigger to programmatically set breakpoint activation

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7455,7 +7455,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-cd60187');
+var VERSION = new core.Version('7.0.0-beta.23-5e54c69');
 
 /**
  * @fileoverview added by tsickle
