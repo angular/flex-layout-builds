189cef895d340aee54cffe7fab70bde11caf4267
Adam Plumer <caerus.karu@gmail.com>
2019-01-15 05:28:52 +0000

    adam/generic - ed61061 refactor(core): create generic sort operations

--- typings/core/media-change.d.ts
+++ typings/core/media-change.d.ts
@@ -22,6 +22,7 @@ export declare class MediaChange {
      * @param mediaQuery e.g. (min-width: 600px) and (max-width: 959px)
      * @param mqAlias e.g. gt-sm, md, gt-lg
      * @param suffix e.g. GtSM, Md, GtLg
+     * @param priority the priority of activation for the given breakpoint
      */
     constructor(matches?: boolean, mediaQuery?: string, mqAlias?: string, suffix?: string, priority?: number);
     /** Create an exact copy of the MediaChange */
