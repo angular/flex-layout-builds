8eeb378b399db0faf72db4f0d9e724e4c11a29c7
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-01-15 04:39:31 +0000

    thomas/fix-media-observer - 34df933 feat(core): MediaObserver can report 1..n activations

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7473,7 +7473,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-ac975b8');
+var VERSION = new core.Version('7.0.0-beta.23-34df933');
 
 /**
  * @fileoverview added by tsickle
