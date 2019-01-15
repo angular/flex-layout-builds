0fbdc55f4fcd09aad7cbd21f83b4815f1b17733c
Adam Plumer <caerus.karu@gmail.com>
2019-01-15 20:20:23 +0000

    adam/mmi - c8d5c61 feat(core): move MatchMedia to internal scope

--- bundles/flex-layout-server.umd.js
+++ bundles/flex-layout-server.umd.js
@@ -6,10 +6,298 @@
  * found in the LICENSE file at https://angular.io/license
  */
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/common'), require('@angular/platform-server'), require('@angular/flex-layout/core'), require('@angular/core')) :
-	typeof define === 'function' && define.amd ? define('@angular/flex-layout/server', ['exports', '@angular/common', '@angular/platform-server', '@angular/flex-layout/core', '@angular/core'], factory) :
-	(factory((global.ng = global.ng || {}, global.ng.flexLayout = global.ng.flexLayout || {}, global.ng.flexLayout.server = {}),global.ng.common,global.ng.platformServer,global.ng.flexLayout.core,global.ng.core));
-}(this, (function (exports,common,platformServer,core,core$1) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/common'), require('@angular/core'), require('@angular/flex-layout/core'), require('@angular/platform-server')) :
+	typeof define === 'function' && define.amd ? define('@angular/flex-layout/server', ['exports', '@angular/common', '@angular/core', '@angular/flex-layout/core', '@angular/platform-server'], factory) :
+	(factory((global.ng = global.ng || {}, global.ng.flexLayout = global.ng.flexLayout || {}, global.ng.flexLayout.server = {}),global.ng.common,global.ng.core,global.ng.flexLayout.core,global.ng.platformServer));
+}(this, (function (exports,common,core,core$1,platformServer) { 'use strict';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/**
+ * Special server-only class to simulate a MediaQueryList and
+ * - supports manual activation to simulate mediaQuery matching
+ * - manages listeners
+ */
+var /**
+ * Special server-only class to simulate a MediaQueryList and
+ * - supports manual activation to simulate mediaQuery matching
+ * - manages listeners
+ */
+ServerMediaQueryList = /** @class */ (function () {
+    function ServerMediaQueryList(_mediaQuery) {
+        this._mediaQuery = _mediaQuery;
+        this._isActive = false;
+        this._listeners = [];
+        this.onchange = null;
+    }
+    Object.defineProperty(ServerMediaQueryList.prototype, "matches", {
+        get: /**
+         * @return {?}
+         */
+        function () {
+            return this._isActive;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ServerMediaQueryList.prototype, "media", {
+        get: /**
+         * @return {?}
+         */
+        function () {
+            return this._mediaQuery;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     * Destroy the current list by deactivating the
+     * listeners and clearing the internal list
+     */
+    /**
+     * Destroy the current list by deactivating the
+     * listeners and clearing the internal list
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.destroy = /**
+     * Destroy the current list by deactivating the
+     * listeners and clearing the internal list
+     * @return {?}
+     */
+    function () {
+        this.deactivate();
+        this._listeners = [];
+    };
+    /** Notify all listeners that 'matches === TRUE' */
+    /**
+     * Notify all listeners that 'matches === TRUE'
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.activate = /**
+     * Notify all listeners that 'matches === TRUE'
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        if (!this._isActive) {
+            this._isActive = true;
+            this._listeners.forEach(function (callback) {
+                /** @type {?} */
+                var cb = /** @type {?} */ ((callback));
+                cb.call(null, _this);
+            });
+        }
+        return this;
+    };
+    /** Notify all listeners that 'matches === false' */
+    /**
+     * Notify all listeners that 'matches === false'
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.deactivate = /**
+     * Notify all listeners that 'matches === false'
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        if (this._isActive) {
+            this._isActive = false;
+            this._listeners.forEach(function (callback) {
+                /** @type {?} */
+                var cb = /** @type {?} */ ((callback));
+                cb.call(null, _this);
+            });
+        }
+        return this;
+    };
+    /** Add a listener to our internal list to activate later */
+    /**
+     * Add a listener to our internal list to activate later
+     * @param {?} listener
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.addListener = /**
+     * Add a listener to our internal list to activate later
+     * @param {?} listener
+     * @return {?}
+     */
+    function (listener) {
+        if (this._listeners.indexOf(listener) === -1) {
+            this._listeners.push(listener);
+        }
+        if (this._isActive) {
+            /** @type {?} */
+            var cb = /** @type {?} */ ((listener));
+            cb.call(null, this);
+        }
+    };
+    /** Don't need to remove listeners in the server environment */
+    /**
+     * Don't need to remove listeners in the server environment
+     * @param {?} _
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.removeListener = /**
+     * Don't need to remove listeners in the server environment
+     * @param {?} _
+     * @return {?}
+     */
+    function (_) {
+    };
+    /**
+     * @param {?} _
+     * @param {?} __
+     * @param {?=} ___
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.addEventListener = /**
+     * @param {?} _
+     * @param {?} __
+     * @param {?=} ___
+     * @return {?}
+     */
+    function (_, __, ___) {
+    };
+    /**
+     * @param {?} _
+     * @param {?} __
+     * @param {?=} ___
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.removeEventListener = /**
+     * @param {?} _
+     * @param {?} __
+     * @param {?=} ___
+     * @return {?}
+     */
+    function (_, __, ___) {
+    };
+    /**
+     * @param {?} _
+     * @return {?}
+     */
+    ServerMediaQueryList.prototype.dispatchEvent = /**
+     * @param {?} _
+     * @return {?}
+     */
+    function (_) {
+        return false;
+    };
+    return ServerMediaQueryList;
+}());
+/**
+ * Special server-only implementation of MatchMedia that uses the above
+ * ServerMediaQueryList as its internal representation
+ *
+ * Also contains methods to activate and deactivate breakpoints
+ */
+var ServerMatchMedia = /** @class */ (function (_super) {
+    __extends(ServerMatchMedia, _super);
+    function ServerMatchMedia(_zone, _platformId, _document) {
+        var _this = _super.call(this, _zone, _platformId, _document) || this;
+        _this._zone = _zone;
+        _this._platformId = _platformId;
+        _this._document = _document;
+        _this._registry = new Map();
+        return _this;
+    }
+    /** Activate the specified breakpoint if we're on the server, no-op otherwise */
+    /**
+     * Activate the specified breakpoint if we're on the server, no-op otherwise
+     * @param {?} bp
+     * @return {?}
+     */
+    ServerMatchMedia.prototype.activateBreakpoint = /**
+     * Activate the specified breakpoint if we're on the server, no-op otherwise
+     * @param {?} bp
+     * @return {?}
+     */
+    function (bp) {
+        /** @type {?} */
+        var lookupBreakpoint = this._registry.get(bp.mediaQuery);
+        if (lookupBreakpoint) {
+            lookupBreakpoint.activate();
+        }
+    };
+    /** Deactivate the specified breakpoint if we're on the server, no-op otherwise */
+    /**
+     * Deactivate the specified breakpoint if we're on the server, no-op otherwise
+     * @param {?} bp
+     * @return {?}
+     */
+    ServerMatchMedia.prototype.deactivateBreakpoint = /**
+     * Deactivate the specified breakpoint if we're on the server, no-op otherwise
+     * @param {?} bp
+     * @return {?}
+     */
+    function (bp) {
+        /** @type {?} */
+        var lookupBreakpoint = this._registry.get(bp.mediaQuery);
+        if (lookupBreakpoint) {
+            lookupBreakpoint.deactivate();
+        }
+    };
+    /**
+     * Call window.matchMedia() to build a MediaQueryList; which
+     * supports 0..n listeners for activation/deactivation
+     */
+    /**
+     * Call window.matchMedia() to build a MediaQueryList; which
+     * supports 0..n listeners for activation/deactivation
+     * @param {?} query
+     * @return {?}
+     */
+    ServerMatchMedia.prototype.buildMQL = /**
+     * Call window.matchMedia() to build a MediaQueryList; which
+     * supports 0..n listeners for activation/deactivation
+     * @param {?} query
+     * @return {?}
+     */
+    function (query) {
+        return new ServerMediaQueryList(query);
+    };
+    ServerMatchMedia.decorators = [
+        { type: core.Injectable },
+    ];
+    /** @nocollapse */
+    ServerMatchMedia.ctorParameters = function () { return [
+        { type: core.NgZone },
+        { type: Object, decorators: [{ type: core.Inject, args: [core.PLATFORM_ID,] }] },
+        { type: undefined, decorators: [{ type: core.Inject, args: [common.DOCUMENT,] }] }
+    ]; };
+    return ServerMatchMedia;
+}(core$1.ɵMatchMedia));
 
 /**
  * @fileoverview added by tsickle
@@ -31,7 +319,7 @@ function generateStaticFlexLayoutStyles(serverSheet, mediaController, breakpoint
     var defaultStyles = new Map(serverSheet.stylesheet);
     /** @type {?} */
     var styleText = generateCss(defaultStyles, 'all', classMap);
-    breakpoints.slice().sort(core.sortAscendingPriority).forEach(function (bp, i) {
+    breakpoints.slice().sort(core$1.sortAscendingPriority).forEach(function (bp, i) {
         serverSheet.clearStyles();
         (/** @type {?} */ (mediaController)).activateBreakpoint(bp);
         /** @type {?} */
@@ -58,7 +346,7 @@ function FLEX_SSR_SERIALIZER_FACTORY(serverSheet, matchMedia, _document, breakpo
         var styleTag = _document.createElement('style');
         /** @type {?} */
         var styleText = generateStaticFlexLayoutStyles(serverSheet, matchMedia, breakpoints);
-        styleTag.classList.add(core.CLASS_NAME + "ssr");
+        styleTag.classList.add(core$1.CLASS_NAME + "ssr");
         styleTag.textContent = styleText; /** @type {?} */
         ((_document.head)).appendChild(styleTag);
     };
@@ -71,20 +359,20 @@ var SERVER_PROVIDERS = [
         provide: /** @type {?} */ (platformServer.BEFORE_APP_SERIALIZED),
         useFactory: FLEX_SSR_SERIALIZER_FACTORY,
         deps: [
-            core.StylesheetMap,
-            core.ɵMatchMedia,
+            core$1.StylesheetMap,
+            core$1.ɵMatchMedia,
             common.DOCUMENT,
-            core.BREAKPOINTS,
+            core$1.BREAKPOINTS,
         ],
         multi: true
     },
     {
-        provide: core.SERVER_TOKEN,
+        provide: core$1.SERVER_TOKEN,
         useValue: true
     },
     {
-        provide: core.ɵMatchMedia,
-        useClass: core.ɵServerMatchMedia
+        provide: core$1.ɵMatchMedia,
+        useClass: ServerMatchMedia
     }
 ];
 /** @type {?} */
@@ -157,7 +445,7 @@ function getClassName(element, classMap) {
     /** @type {?} */
     var className = classMap.get(element);
     if (!className) {
-        className = "" + core.CLASS_NAME + nextId++;
+        className = "" + core$1.CLASS_NAME + nextId++;
         classMap.set(element, className);
     }
     element.classList.add(className);
@@ -172,7 +460,7 @@ var FlexLayoutServerModule = /** @class */ (function () {
     function FlexLayoutServerModule() {
     }
     FlexLayoutServerModule.decorators = [
-        { type: core$1.NgModule, args: [{
+        { type: core.NgModule, args: [{
                     providers: [SERVER_PROVIDERS]
                 },] },
     ];
@@ -183,6 +471,7 @@ exports.FlexLayoutServerModule = FlexLayoutServerModule;
 exports.generateStaticFlexLayoutStyles = generateStaticFlexLayoutStyles;
 exports.FLEX_SSR_SERIALIZER_FACTORY = FLEX_SSR_SERIALIZER_FACTORY;
 exports.SERVER_PROVIDERS = SERVER_PROVIDERS;
+exports.ɵa1 = ServerMatchMedia;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- typings/esm5/core/public-api.d.ts
+++ typings/esm5/core/public-api.d.ts
@@ -13,7 +13,7 @@ export * from './tokens/index';
 export * from './add-alias';
 export * from './base/index';
 export * from './breakpoints/index';
-export { MatchMedia as ɵMatchMedia, ServerMatchMedia as ɵServerMatchMedia, MockMatchMedia as ɵMockMatchMedia, MockMatchMediaProvider as ɵMockMatchMediaProvider, } from './match-media/index';
+export { MatchMedia as ɵMatchMedia, MockMatchMedia as ɵMockMatchMedia, MockMatchMediaProvider as ɵMockMatchMediaProvider, } from './match-media/index';
 export * from './media-observer/index';
 export * from './utils/index';
 export * from './style-utils/style-utils';
