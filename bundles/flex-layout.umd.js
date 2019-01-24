1f9e047f257f618916da47a3c6f73f7ab1733837
Nathan Taylor <taylonr@gmail.com>
2019-01-24 16:58:27 +0000

    master - 8cf36ce docs: add a Getting Started section to the README (#1007)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -7529,7 +7529,7 @@ var GridModule = /** @class */ (function () {
 /** *
  * Current version of Angular Flex-Layout.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.23-6f43cf6');
+var VERSION = new core.Version('7.0.0-beta.23-8cf36ce');
 
 /**
  * @fileoverview added by tsickle
