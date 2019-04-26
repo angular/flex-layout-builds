d40f25501ed383967f7c8be8c4aa6f6339e74de2
CaerusKaru <caerus.karu@gmail.com>
2019-04-26 17:43:26 +0000

    master - e297426 build: add `@angular/platform-browser` as a peer dep (#1054)

--- core/typings/media-observer/media-observer.d.ts
+++ core/typings/media-observer/media-observer.d.ts
@@ -58,7 +58,7 @@ export declare class MediaObserver implements OnDestroy {
     protected hook: PrintHook;
     /**
      * @deprecated Use `asObservable()` instead.
-     * @breaking-change 7.0.0-beta.24-b2e2064
+     * @breaking-change 7.0.0-beta.24-e297426
      * @deletion-target v7.0.0-beta.25
      */
     readonly media$: Observable<MediaChange>;
