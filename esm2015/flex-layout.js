720938880d63fb5c858c2a6f63e8c3b2d8ae34b9
Adam Plumer <caerus.karu@gmail.com>
2019-05-24 22:13:44 +0000

    8.0.0-beta.26 - 5755258 build: upgrade to Angular v8 final RC w/ release fix

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -24,7 +24,7 @@ export { ɵf2, ɵe2, ɵd2, ɵi2, ɵh2, ɵg2, ɵl2, ɵk2, ɵj2, ɵo2, ɵn2, ɵm2,
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.26-563786a');
+const VERSION = new Version('8.0.0-beta.26-5755258');
 
 /**
  * @fileoverview added by tsickle
