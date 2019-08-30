a2f32173c3c683e68bc54a3eb1881e24c8918458
Adam Plumer <caerus.karu@gmail.com>
2019-08-30 16:35:25 +0000

    adam/8.0.0-beta.27 - 14987fe chore: update to 8.0.0-beta.27 with changelog

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8439,7 +8439,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.26-2d7b49c');
+var VERSION = new core.Version('8.0.0-beta.27-14987fe');
 
 /**
  * @fileoverview added by tsickle
