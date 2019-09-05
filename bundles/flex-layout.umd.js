2f380e756915cffdaa8a55199583e11d344e22f5
Joey Perrott <josephperrott.github@gmail.com>
2019-09-05 21:12:33 +0000

    master - f00e232 chore: move to latest lock-closed commit for github action (#1117)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8439,7 +8439,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.27-03f7baa');
+var VERSION = new core.Version('8.0.0-beta.27-f00e232');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -25,7 +25,7 @@ export { ɵf2, ɵe2, ɵd2, ɵi2, ɵh2, ɵg2, ɵl2, ɵk2, ɵj2, ɵo2, ɵn2, ɵm2,
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-beta.27-03f7baa');
+var VERSION = new Version('8.0.0-beta.27-f00e232');
 
 /**
  * @fileoverview added by tsickle
