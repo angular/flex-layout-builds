121a6853ae0d25771c31689ae992c889d1ed7190
Joey Perrott <josephperrott.github@gmail.com>
2019-09-10 14:07:25 +0000

    master - 951ce4f chore: update lock-closed to only run once per day (#1121)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8439,7 +8439,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.27-f00e232');
+var VERSION = new core.Version('8.0.0-beta.27-951ce4f');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -24,7 +24,7 @@ export { ɵf2, ɵe2, ɵd2, ɵi2, ɵh2, ɵg2, ɵl2, ɵk2, ɵj2, ɵo2, ɵn2, ɵm2,
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.27-f00e232');
+const VERSION = new Version('8.0.0-beta.27-951ce4f');
 
 /**
  * @fileoverview added by tsickle
