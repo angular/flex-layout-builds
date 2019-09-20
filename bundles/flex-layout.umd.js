fc11ef692b1b5918b48e211bf4f8b5b46d6a7976
CaerusKaru <caerus.karu@gmail.com>
2019-09-20 03:36:02 +0000

    master - 7dcc14d chore: remove package-lock (#1125)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8439,7 +8439,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.27-951ce4f');
+var VERSION = new core.Version('8.0.0-beta.27-7dcc14d');
 
 /**
  * @fileoverview added by tsickle
