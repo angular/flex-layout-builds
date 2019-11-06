6de46b5e49b8c206ae0f71cfb872241153c11218
Adam Plumer <caerus.karu@gmail.com>
2019-11-06 06:00:29 +0000

    adam/fix-issue-1079 - 7c474a5 build: fix gulp setup not working with node v12

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8439,7 +8439,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.27-42aaefa');
+var VERSION = new core.Version('8.0.0-beta.27-7c474a5');
 
 /**
  * @fileoverview added by tsickle
