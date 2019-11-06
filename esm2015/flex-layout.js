00a08bc47b066458176a7f349faa1c03061dba2e
Adam Plumer <caerus.karu@gmail.com>
2019-11-06 05:47:36 +0000

    adam/fix-issue-1079 - 42aaefa build: fix gulp setup not working with node v12

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -24,7 +24,7 @@ export { ɵf2, ɵe2, ɵd2, ɵi2, ɵh2, ɵg2, ɵl2, ɵk2, ɵj2, ɵo2, ɵn2, ɵm2,
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.27-06268b8');
+const VERSION = new Version('8.0.0-beta.27-42aaefa');
 
 /**
  * @fileoverview added by tsickle
