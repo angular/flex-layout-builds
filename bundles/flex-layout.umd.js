da9c61fd21d736bc1c4cbf198d13be82003c96ef
CaerusKaru <caerus.karu@gmail.com>
2020-01-27 19:12:13 +0000

    master - f8e9d75 chore: update to 9.0.0-beta.28 with changelog (#1179)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -8466,7 +8466,7 @@ var GridModule = /** @class */ (function () {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.27-8099fca');
+var VERSION = new core.Version('9.0.0-beta.28-f8e9d75');
 
 /**
  * @fileoverview added by tsickle
