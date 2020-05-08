e3cff5f1e871f05187a13102d6cc2f8fd37aeca1
CaerusKaru <caerus.karu@gmail.com>
2020-05-08 21:37:57 +0000

    master - bf2355b fix(show-hide): use initial value as fallback instead of parent (#1243)

--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('9.0.0-beta.29-e8ded3e');
+var VERSION = new Version('9.0.0-beta.29-bf2355b');
 
 /**
  * @fileoverview added by tsickle
