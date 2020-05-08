4d7bb2f93d4063b2df75b795f4c0d6a31c594fbe
LaurieCuriant <58851093+LaurieCuriant@users.noreply.github.com>
2020-05-08 00:01:01 +0000

    master - 9ddbc43 docs: fix HTML closing tags for fxFlex API page (#1225)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.29-93a426a');
+var VERSION = new core.Version('9.0.0-beta.29-9ddbc43');
 
 /**
  * @fileoverview added by tsickle
