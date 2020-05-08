bfc64055080bee71e4aec5c5b023e94b3b332a16
Adam Plumer <caerus.karu@gmail.com>
2020-05-08 00:07:55 +0000

    adam/close-1180 - 2d77d08 build: use no async option for ngcc

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.29-9ddbc43');
+var VERSION = new core.Version('9.0.0-beta.29-2d77d08');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('9.0.0-beta.29-9ddbc43');
+var VERSION = new Version('9.0.0-beta.29-2d77d08');
 
 /**
  * @fileoverview added by tsickle
