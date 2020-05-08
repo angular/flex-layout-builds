0773e43fe61d183da1dc8d9e3317f655aa807e2b
Adam Plumer <caerus.karu@gmail.com>
2020-05-08 21:26:18 +0000

    adam/fix-issue-1163 - 4012b17 fix(show-hide): use initial value as fallback instead of parent

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.29-e8ded3e');
+var VERSION = new core.Version('9.0.0-beta.29-4012b17');
 
 /**
  * @fileoverview added by tsickle
