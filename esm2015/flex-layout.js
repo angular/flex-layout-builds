55f14fae8fd8304ec5e7461a754b0dd8f8ca6d4c
CaerusKaru <caerus.karu@gmail.com>
2020-05-11 18:16:28 +0000

    master - 942c507 chore: update to 9.0.0-beta.30 with changelog (#1248)

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -25,7 +25,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-beta.29-2ca7848');
+const VERSION = new Version('9.0.0-beta.30-942c507');
 
 /**
  * @fileoverview added by tsickle
