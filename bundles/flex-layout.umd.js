4b9ee8075b4853ebed7b1ecd77eacf302b60d3db
CaerusKaru <caerus.karu@gmail.com>
2020-05-15 18:19:08 +0000

    master - fc0b4f5 chore: update to 9.0.0-beta.31 with changelog (#1257)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.30-9d957eb');
+var VERSION = new core.Version('9.0.0-beta.31-fc0b4f5');
 
 /**
  * @fileoverview added by tsickle
