2571036ebadfd582d4b213eb8fb57a8270e9ef1a
Adam Plumer <caerus.karu@gmail.com>
2020-05-22 01:35:57 +0000

    3.9 - 704bf4b build: upgrade to Angular v10, CDK v10, and TypeScript 3.9

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.31-d337620');
+var VERSION = new core.Version('9.0.0-beta.31-704bf4b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('9.0.0-beta.31-d337620');
+var VERSION = new Version('9.0.0-beta.31-704bf4b');
 
 /**
  * @fileoverview added by tsickle
