677bf95aa766b349d69c485e5d6463f55daa988f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-22 10:23:15 +0000

    3.9 - a8b1c4f build: preserve symlinks when building nested CLI apps

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.31-704bf4b');
+var VERSION = new core.Version('9.0.0-beta.31-a8b1c4f');
 
 /**
  * @fileoverview added by tsickle
