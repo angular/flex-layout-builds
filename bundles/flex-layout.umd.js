011c88d3c9fea5996f8f44c91668de3b78bcc86c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-22 13:32:08 +0000

    3.9 - 60d4c4e build: preserve symlinks when building nested CLI apps

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.31-a8b1c4f');
+var VERSION = new core.Version('9.0.0-beta.31-60d4c4e');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('9.0.0-beta.31-a8b1c4f');
+var VERSION = new Version('9.0.0-beta.31-60d4c4e');
 
 /**
  * @fileoverview added by tsickle
