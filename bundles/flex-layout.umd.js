7f36c7ba975202be09e918bcee33fd54535b7fe8
CaerusKaru <caerus.karu@gmail.com>
2020-06-16 19:16:08 +0000

    master - 7e7912c chore: update to 10.0.0-beta.32 with changelog (#1272)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('9.0.0-beta.31-9d73393');
+var VERSION = new core.Version('10.0.0-beta.32-7e7912c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('9.0.0-beta.31-9d73393');
+var VERSION = new Version('10.0.0-beta.32-7e7912c');
 
 /**
  * @fileoverview added by tsickle
