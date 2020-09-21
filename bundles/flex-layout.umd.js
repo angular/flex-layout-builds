919f264be673812c184ca3bce05c9a73581b457b
Joey Perrott <josephperrott.github@gmail.com>
2020-09-21 20:35:55 +0000

    master - 0621f58 build: update stylelint usage to remove blacklist and whitelist (#1296)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('10.0.0-beta.32-381b7c5');
+var VERSION = new core.Version('10.0.0-beta.32-0621f58');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -25,7 +25,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-beta.32-381b7c5');
+const VERSION = new Version('10.0.0-beta.32-0621f58');
 
 /**
  * @fileoverview added by tsickle
