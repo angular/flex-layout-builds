be241e7e0fa981e52e798727944c2419be61b300
CaerusKaru <caerus.karu@gmail.com>
2020-11-12 07:39:36 +0000

    fbp - 8c93571 fix(core): set fractional breakpoints to .98 instead of .9

--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('10.0.0-beta.32-4395c75');
+var VERSION = new Version('10.0.0-beta.32-8c93571');
 
 /**
  * @fileoverview added by tsickle
