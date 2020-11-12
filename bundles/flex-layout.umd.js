99683bdc45a5fe488d551774f15100e41ed65b2e
CaerusKaru <caerus.karu@gmail.com>
2020-11-12 07:12:19 +0000

    v11 - 40fec43 build: upgrade to Angular v11, CDK v11, and TypeScript 4.0

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('10.0.0-beta.32-059072e');
+var VERSION = new core.Version('10.0.0-beta.32-40fec43');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -25,7 +25,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-beta.32-059072e');
+const VERSION = new Version('10.0.0-beta.32-40fec43');
 
 /**
  * @fileoverview added by tsickle
