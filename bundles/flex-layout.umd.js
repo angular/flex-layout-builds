fd25d41c97854de2ba1efc2d89373ec7951b6bcf
CaerusKaru <caerus.karu@gmail.com>
2021-03-30 19:52:29 +0000

    master - 623bc14 ci: update sauceconnect verstion to 4.6.2 (#1331)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('11.0.0-beta.33-7580662');
+var VERSION = new core.Version('11.0.0-beta.33-623bc14');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { ɵgrid_privatef, ɵgrid_privatee, ɵgrid_privated, ɵgrid_privatei, ɵg
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('11.0.0-beta.33-7580662');
+var VERSION = new Version('11.0.0-beta.33-623bc14');
 
 /**
  * @fileoverview added by tsickle
