9060c30550362d80120f77be4522b27c98936e9a
Duncan <info@e-ips.com>
2021-08-03 14:40:38 +0000

    master - 4022c08 Media observer docs (#1354)

--- bundles/flex-layout.umd.js
+++ bundles/flex-layout.umd.js
@@ -46,7 +46,7 @@ var __assign = function() {
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new core.Version('12.0.0-beta.34-2c1b959');
+var VERSION = new core.Version('12.0.0-beta.34-4022c08');
 
 /**
  * @fileoverview added by tsickle
--- esm5/flex-layout.es5.js
+++ esm5/flex-layout.es5.js
@@ -26,7 +26,7 @@ export { GridModule, GridAlignColumnsStyleBuilder, GridAlignColumnsDirective, De
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-var VERSION = new Version('12.0.0-beta.34-2c1b959');
+var VERSION = new Version('12.0.0-beta.34-4022c08');
 
 /**
  * @fileoverview added by tsickle
