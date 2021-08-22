35bc961b7a4ec2983bff6f069f9e4fbb42d7b591
CaerusKaru <caerus.karu@gmail.com>
2021-08-22 00:24:06 +0000

    master - bd42b2b build: allow for rxjs v7 usage (#1359)

--- esm2015/flex-layout.js
+++ esm2015/flex-layout.js
@@ -25,7 +25,7 @@ export { GridModule, GridAlignColumnsStyleBuilder, GridAlignColumnsDirective, De
  * Current version of Angular Flex-Layout.
  * @type {?}
  */
-const VERSION = new Version('12.0.0-beta.34-e8493da');
+const VERSION = new Version('12.0.0-beta.34-bd42b2b');
 
 /**
  * @fileoverview added by tsickle
