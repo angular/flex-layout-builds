dd93663f7971b10c90109ad12a3b3d3b79b12b1f
CaerusKaru <caerus.karu@gmail.com>
2021-11-29 18:12:59 +0000

    master - 4ea6714 chore: update to 13.0.0-beta.36 with changelog (#1373)

--- fesm2020/angular-flex-layout.mjs
+++ fesm2020/angular-flex-layout.mjs
@@ -18,7 +18,7 @@ import { isPlatformServer } from '@angular/common';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Flex-Layout. */
-const VERSION = new Version('12.0.0-beta.35-1be539b');
+const VERSION = new Version('13.0.0-beta.36-4ea6714');
 
 /**
  * @license
